9ad9898a3a27cfefbf1e170b32f3673e9e3096d4
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2019-12-06 11:42:40 -0500

    added notes to second hook useLocalStorage.js

--- client/src/hooks/useLocalStorage.js
+++ client/src/hooks/useLocalStorage.js
@@ -15,3 +15,8 @@ const useLocalStorage = (key, initialValue) => {
 };
 
 export default useLocalStorage;
+
+// NOtes ****
+// Although this isn’t something you will often do (storing input values in localStorage), this setup is quite powerful, and it effectively demonstrates how composable hooks can be; by combining the stateful logic of multiple custom hooks, you can compose a really nice custom hook with advanced stateful logic.
+
+// One final thing to note is that we can employ the useLocalStorage custom hook in other places now as well. So, not only do we have an extra-powerful useInput created by composing multiple hooks together, we also have another custom hook available to us anytime we want to persist data in localStorage.
