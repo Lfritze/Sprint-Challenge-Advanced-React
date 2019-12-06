41a72491023274c60c2e6893e8d9a708a10923e1
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2019-12-06 11:37:13 -0500

    setup change color mode (dark-mode) toggle and added change in App.css

--- client/src/components/NavHeader.js
+++ client/src/components/NavHeader.js
@@ -1,13 +1,23 @@
 import React from "react";
+import useDarkMode from "../hooks/useDarkMode";
 
 const NavHeader = () => {
+  const [toggle, setToggle] = useDarkMode(false);
+
+  const darkToggle = e => {
+    e.preventDefault();
+    setToggle(!toggle);
+  };
+
   return (
     <div>
       <h2>
         Women's World Cup <br></br>Search Data
       </h2>
       <div>
-        <button data-testid="navbutton">Change Color Mode</button>
+        <button data-testid="navbutton" onClick={darkToggle}>
+          Change Color Mode
+        </button>
       </div>
     </div>
   );
---
+++ client/src/hooks/useLocalStorage.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+const useLocalStorage = (key, initialValue) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    const entry = window.localStorage.getItem(key);
+    return entry ? JSON.parse(entry) : initialValue;
+  });
+
+  const setValue = value => {
+    console.log(value); // logs true or false
+    setStoredValue(value);
+    window.localStorage.setItem(key, JSON.stringify(value));
+  };
+  return [storedValue, setValue];
+};
+
+export default useLocalStorage;
