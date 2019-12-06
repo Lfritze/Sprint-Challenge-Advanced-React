d192c158f82a97df58ddeb974b67e2003921cf63
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2019-12-06 12:10:08 -0500

    added styling for DataCard.js

--- client/src/components/DataCard.js
+++ client/src/components/DataCard.js
@@ -1,14 +1,15 @@
 import React from "react";
+import { SoccerCard } from "../styledComponents/AppStyling";
 
 const DataCard = props => {
   // console.log(props);
   return (
-    <div>
+    <SoccerCard>
       <h1>{props.data.name}</h1>
       <h3>Country: {props.data.country}</h3>
       <p>Google Searches: {props.data.searches}</p>
       <p>ID: {props.data.id}</p>
-    </div>
+    </SoccerCard>
   );
 };
 
---
+++ client/src/styledComponents/AppStyling.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+//DataCard
+
+export const SoccerCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 20%;
+  min-height: 300px;
+  border: 2px solid black;
+  padding: 2%;
+  margin: 2%;
+  background-image: linear-gradient(#b19cd9, #86fde8);
+  border-radius: 50px;
+`;
