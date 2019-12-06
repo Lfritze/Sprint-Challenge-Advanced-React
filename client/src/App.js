19d8b2a24cb79bdc339ed047d3e063845ec6e990
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2019-12-06 11:20:12 -0500

    setup App.js with axios request and cDM

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/api/players")
+      .then(response => {
+        console.log(response);
+        this.setState({ data: response.data });
+      })
+      .catch(error => {
+        console.log("get data error in .catch App.js", error);
+      });
+  }
+  render() {
+    return <div className="App"></div>;
+  }
 }
 
 export default App;
